efca6b710f37c28500a12bc122130c5a90a8ce40
yuangezhizao-MacMini <yuangezhizao@icloud.com>
2021-11-03 23:41:35 +0800

    🐛 Bug(/pages/overview.js): 修复多写了一层 body（禁止套娃

--- pages/overview.js
+++ pages/overview.js
@@ -32,98 +32,96 @@ function Bangumi() {
   return (
     <>
       <Head />
-      <body>
-        <Grid centered>
+      <Grid centered>
+        <Grid.Column wide={12}>
+          <Menu pointing color="blue">
+            <Menu.Item active>
+              <Icon name="tv" />
+              总日漫表
+            </Menu.Item>
+            <Menu.Item href="/">
+              <Icon name="arrow left" />
+              返回首页
+            </Menu.Item>
+          </Menu>
           <Grid.Column wide={12}>
-            <Menu pointing color="blue">
-              <Menu.Item active>
-                <Icon name="tv" />
-                总日漫表
-              </Menu.Item>
-              <Menu.Item href="/">
-                <Icon name="arrow left" />
-                返回首页
-              </Menu.Item>
-            </Menu>
-            <Grid.Column wide={12}>
-              <Header dividing color="blue">
-                <HeaderContent>
-                  <h2>我的所有追番</h2>
-                  <Header sub>
-                    数据来自：<a href="https://bgmlist.com/">番组放送</a>
-                  </Header>
-                </HeaderContent>
-              </Header>
-              <Table compact sortable celled unstackable striped color="blue">
-                <Table.Header>
-                  <Table.Row positive>
-                    <Table.HeaderCell>No.</Table.HeaderCell>
-                    <Table.HeaderCell>放送季度</Table.HeaderCell>
-                    <Table.HeaderCell>番剧名称</Table.HeaderCell>
-                    <Table.HeaderCell>番剧类型</Table.HeaderCell>
-                    <Table.HeaderCell>番剧话数</Table.HeaderCell>
-                    <Table.HeaderCell className="one wide">轻小说</Table.HeaderCell>
-                    <Table.HeaderCell className="one wide">漫画</Table.HeaderCell>
-                    <Table.HeaderCell className="one wide">动画</Table.HeaderCell>
-                    <Table.HeaderCell>追番状态</Table.HeaderCell>
-                    <Table.HeaderCell>追番源</Table.HeaderCell>
-                    <Table.HeaderCell>数据插入时间</Table.HeaderCell>
-                    <Table.HeaderCell>数据更新时间</Table.HeaderCell>
+            <Header dividing color="blue">
+              <HeaderContent>
+                <h2>我的所有追番</h2>
+                <Header sub>
+                  数据来自：<a href="https://bgmlist.com/">番组放送</a>
+                </Header>
+              </HeaderContent>
+            </Header>
+            <Table compact sortable celled unstackable striped color="blue">
+              <Table.Header>
+                <Table.Row positive>
+                  <Table.HeaderCell>No.</Table.HeaderCell>
+                  <Table.HeaderCell>放送季度</Table.HeaderCell>
+                  <Table.HeaderCell>番剧名称</Table.HeaderCell>
+                  <Table.HeaderCell>番剧类型</Table.HeaderCell>
+                  <Table.HeaderCell>番剧话数</Table.HeaderCell>
+                  <Table.HeaderCell className="one wide">轻小说</Table.HeaderCell>
+                  <Table.HeaderCell className="one wide">漫画</Table.HeaderCell>
+                  <Table.HeaderCell className="one wide">动画</Table.HeaderCell>
+                  <Table.HeaderCell>追番状态</Table.HeaderCell>
+                  <Table.HeaderCell>追番源</Table.HeaderCell>
+                  <Table.HeaderCell>数据插入时间</Table.HeaderCell>
+                  <Table.HeaderCell>数据更新时间</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {data.map((each, index) => (
+                  <Table.Row key={data.length - index}>
+                    <Table.Cell>{data.length - index}</Table.Cell>
+                    <Table.Cell>{each.season}</Table.Cell>
+                    <Table.Cell>{each.title}</Table.Cell>
+                    <Table.Cell>{each.type}</Table.Cell>
+                    <Table.Cell>{each.episodes}</Table.Cell>
+                    <Table.Cell>{each.novel}</Table.Cell>
+                    <Table.Cell>{each.comics}</Table.Cell>
+                    <Popup
+                      trigger={
+                        <Table.Cell>
+                          <Image
+                            src={
+                              'https://lab.yuangezhizao.cn/api/v0.0.1/bangumi?animation=' +
+                              each.animation
+                            }
+                            size="tiny"
+                          />
+                        </Table.Cell>
+                      }
+                      content={each.animation}
+                      inverted
+                    />
+                    <Table.Cell>{each.status}</Table.Cell>
+                    <Table.Cell>{each.blu_ray}</Table.Cell>
+                    <Popup
+                      trigger={
+                        <Table.Cell>
+                          {moment(each.insert_time).utc().format('YYYY-MM-DD')}
+                        </Table.Cell>
+                      }
+                      content={moment(each.insert_time).utc().format('YYYY-MM-DD HH:mm:ss')}
+                      inverted
+                    />
+                    <Popup
+                      trigger={
+                        <Table.Cell>
+                          {moment(each.update_time).utc().format('YYYY-MM-DD')}
+                        </Table.Cell>
+                      }
+                      content={moment(each.update_time).utc().format('YYYY-MM-DD HH:mm:ss')}
+                      inverted
+                    />
                   </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                  {data.map((each, index) => (
-                    <Table.Row key={data.length - index}>
-                      <Table.Cell>{data.length - index}</Table.Cell>
-                      <Table.Cell>{each.season}</Table.Cell>
-                      <Table.Cell>{each.title}</Table.Cell>
-                      <Table.Cell>{each.type}</Table.Cell>
-                      <Table.Cell>{each.episodes}</Table.Cell>
-                      <Table.Cell>{each.novel}</Table.Cell>
-                      <Table.Cell>{each.comics}</Table.Cell>
-                      <Popup
-                        trigger={
-                          <Table.Cell>
-                            <Image
-                              src={
-                                'https://lab.yuangezhizao.cn/api/v0.0.1/bangumi?animation=' +
-                                each.animation
-                              }
-                              size="tiny"
-                            />
-                          </Table.Cell>
-                        }
-                        content={each.animation}
-                        inverted
-                      />
-                      <Table.Cell>{each.status}</Table.Cell>
-                      <Table.Cell>{each.blu_ray}</Table.Cell>
-                      <Popup
-                        trigger={
-                          <Table.Cell>
-                            {moment(each.insert_time).utc().format('YYYY-MM-DD')}
-                          </Table.Cell>
-                        }
-                        content={moment(each.insert_time).utc().format('YYYY-MM-DD HH:mm:ss')}
-                        inverted
-                      />
-                      <Popup
-                        trigger={
-                          <Table.Cell>
-                            {moment(each.update_time).utc().format('YYYY-MM-DD')}
-                          </Table.Cell>
-                        }
-                        content={moment(each.update_time).utc().format('YYYY-MM-DD HH:mm:ss')}
-                        inverted
-                      />
-                    </Table.Row>
-                  ))}
-                </Table.Body>
-              </Table>
-            </Grid.Column>
+                ))}
+              </Table.Body>
+            </Table>
           </Grid.Column>
-        </Grid>
-      </body>
+        </Grid.Column>
+      </Grid>
     </>
   );
 }

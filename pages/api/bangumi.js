eee4555b30bfc65437a9725c3d88f7df22892767
yuangezhizao-MacMini <yuangezhizao@icloud.com>
2021-11-13 22:18:49 +0800

    🐛 Bug(/pages/api/bangumi.js): 继续尝试修复

--- pages/api/bangumi.js
+++ pages/api/bangumi.js
@@ -1,12 +1,11 @@
-const fs = require('fs');
-const path = require('path');
 const csv = require('csvtojson');
-
-// https://web.archive.org/web/20211113140226/https://medium.com/@boris.poehland.business/next-js-api-routes-how-to-read-files-from-directory-compatible-with-vercel-5fb5837694b9
-const csvFilePath = path.resolve('./public', 'bangumi/animations.csv');
-
-var readDir = fs.readdirSync('./public');
-console.log(readDir);
+const dev = process.env.NODE_ENV !== 'production';
+var csvFilePath = '';
+if (dev === true) {
+  csvFilePath = './public/bangumi/animations.csv';
+} else {
+  csvFilePath = 'bangumi/animations.csv';
+}
 
 export default async function handler(req, res) {
   const { animation } = req.query;

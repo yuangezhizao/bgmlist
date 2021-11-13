df243b5520467a6de2e0b2a6e37924783f8bc10e
yuangezhizao-MacMini <yuangezhizao@icloud.com>
2021-11-13 21:54:14 +0800

    🐛 Bug(/pages/api/bangumi.js): 继续尝试修复

--- pages/api/bangumi.js
+++ pages/api/bangumi.js
@@ -1,17 +1,19 @@
+const path = require('path');
 const csv = require('csvtojson');
+
 const dev = process.env.NODE_ENV !== 'production';
-var csvFilePath = '';
+let csvFilePath = '';
 if (dev === true) {
   csvFilePath = './public/bangumi/animations.csv';
 } else {
-  csvFilePath = '/bangumi/animations.csv';
+  csvFilePath = path.join(process.cwd(), 'public/bangumi/animations.csv');
 }
 
 export default async function handler(req, res) {
   const { animation } = req.query;
   const jsonArray = await csv().fromFile(csvFilePath);
   // console.log(animation);
-  var result = jsonArray.filter(function (fp) {
+  let result = jsonArray.filter(function (fp) {
     return fp.name === animation;
   });
   // console.log(result[0])

7c7c4f61e6462510c8cf955040e6ccf8b104024f
yuangezhizao-MacMini <yuangezhizao@icloud.com>
2021-12-10 18:16:36 +0800

    🐛 Bug(/pages/api/bangumi.js): 继续尝试修复
    参照：https://github.com/vercel/next.js/discussions/32236?sort=top#discussioncomment-1766808

--- pages/api/bangumi.js
+++ pages/api/bangumi.js
@@ -1,9 +1,13 @@
+import path from 'path';
+import getConfig from 'next/config';
+
 const csv = require('csvtojson');
 const csvFilePath = './public/bangumi/animations.csv';
+const { serverRuntimeConfig } = getConfig();
 
 export default async function handler(req, res) {
   const { animation } = req.query;
-  const jsonArray = await csv().fromFile(csvFilePath);
+  const jsonArray = await csv().fromFile(path.join(serverRuntimeConfig.PROJECT_ROOT, csvFilePath));
   // console.log(animation);
   let result = jsonArray.filter(function (fp) {
     return fp.name === animation;

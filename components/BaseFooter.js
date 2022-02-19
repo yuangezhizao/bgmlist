c95870328ce26aeba61c17c3a01b7a63da4e914a
yuangezhizao-MacMini <yuangezhizao@icloud.com>
2022-02-19 22:48:20 +0800

    ♻️  Refactoring(/components/basefooter.js): 区分部署服务商（不能一直给你云打广告，又木有给广告费 2333；追加 Git Commit Hash 显示

--- components/BaseFooter.js
+++ components/BaseFooter.js
@@ -7,7 +7,17 @@ function BaseFooter() {
         <Message info>
           <MessageContent>
             yuangezhizao © 2021 - 2022<br></br>
-            Powered by Tencent Cloud Serverless
+            Powered by {process.env.VERCEL === 1 ? 'Vercel' : 'Tencent Cloud Serverless'}
+            {process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA && (
+              <>
+                <br></br>
+                <a
+                  href={
+                    'https://github.com/yuangezhizao/bgmlist/commit/' +
+                    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA
+                  }>#{process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA.substring(0, 7)}</a>
+              </>
+            )}
           </MessageContent>
         </Message>
       </footer>
